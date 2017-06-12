ac55ac2ee7a5ebb66120d98d44b61ea148d423a7
GG1991 <giuntoli1991@gmail.com>
2017-06-12 16:43:18 +0200

    bug fix on Bash script for debugging coupling

--- examples/fuel_couple/debug_coupling.sh
+++ examples/fuel_couple/debug_coupling.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-#set -x
+bps=( 'fer_couple.c:50' 'fer_couple.c:51') 
+
+
 # BREAKPOINTS
-bps=( "fer_couple.c:50" ) 
-
-
-# BREAKPOINTS COMMANDS
-exopt=
-for i in "${bps[@]}"
+for i in ${bps[@]}
 do
-exopt="$exopt -ex 'break $i'"
+  exopt="$exopt -ex 'break $i' "
 done
 
-# PRETTY OPTION
-#exopt="$exopt -ex 'set print pretty on'"
-
 echo $exopt
-gdbcomm="gdb "$exopt" --args  ../../fermi fuel_1.fer -c couple.dat"
+gdbcomm="gdb $exopt --args  ../../fermi fuel_1.fer -c couple.dat"
 echo $gdbcomm
 
-#set +x
-
-#mpirun -np 1 xterm -e $gdbcomm : -np 1 xterm -e gdb control
-
-mpirun -np 1 xterm -e gdb -ex 'break fer_couple.c:50'                    \
-                          -ex 'set print pretty on'                      \
-			  --args  ../../fermi fuel_1.fer -c couple.dat : \
-       -np 1 xterm -e gdb -ex 'break control.c:105' control
-
-#a="-ex 'break fer_couple.c:50'"
-#mpirun -np 1 xterm -e gdb $a                                             \
-#                          -ex 'set print pretty on'                      \
-#			  --args  ../../fermi fuel_1.fer -c couple.dat : \
-#       -np 1 xterm -e gdb -ex 'break control.c:105' control
+mpirun -np 1 xterm -e "$gdbcomm" : -np 1 xterm -e gdb control
